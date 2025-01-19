32509253d2744ee3831242c2aa45c6fd9b79c565
david_smith <david_smith@sweetwater.com>
2025-01-18 20:26:32 -0500

    feat/add-swagger Created `tests/Unit/Swagger/HostTest.php`.

---
+++ tests/Unit/Swagger/HostTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Swagger;
+
+use Factories\InfoFactory;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+use Zerotoprod\DataModelSwagger\Swagger;
+
+class HostTest extends TestCase
+{
+    #[Test] public function null(): void
+    {
+        $Swagger = Swagger::from([
+            Swagger::swagger => Swagger::swagger,
+            Swagger::info => InfoFactory::factory()->make(),
+            Swagger::paths => [],
+        ]);
+
+        self::assertNull(
+            actual: $Swagger->host,
+            message: 'The host (name or ip) serving the API. This MUST be the host only and does not include the scheme nor sub-paths. It MAY include a port. If the host is not included, the host serving the documentation is to be used (including the port). The host does not support path templating.'
+        );
+    }
+    #[Test] public function host(): void
+    {
+        $Swagger = Swagger::from([
+            Swagger::swagger => Swagger::swagger,
+            Swagger::info => InfoFactory::factory()->make(),
+            Swagger::paths => [],
+        ]);
+
+        self::assertEquals(
+            expected: Swagger::swagger,
+            actual: $Swagger->swagger,
+            message: 'The host (name or ip) serving the API. This MUST be the host only and does not include the scheme nor sub-paths. It MAY include a port. If the host is not included, the host serving the documentation is to be used (including the port). The host does not support path templating.'
+        );
+    }
+}
